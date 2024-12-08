c4523270d0d9743311f30cc05c8f3ccb94d25a83
ron1n101 <daticob@gmail.com>
2024-12-08 21:19:18 +0200

    Added main.cpp

---
+++ Server_Qt_courseWork/main.cpp
@@ -0,0 +1,20 @@
+#include "Server.h"
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+
+    Server server;
+    if(!server.listen(QHostAddress::LocalHost, 8001))
+    {
+        qCritical() << "Failed to start server: " << server.errorString();
+        return -1;
+    }
+
+    qDebug() << "Server started on port 8001";
+    return a.exec();
+}
